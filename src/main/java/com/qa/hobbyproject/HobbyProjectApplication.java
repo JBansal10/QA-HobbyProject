79d2bf3f7756b821f7de547ce581a6de612e2865
Jbansal10 <jbansal@academytrainee.com>
2021-01-20 13:59:42 +0000

    starting on firstentity

--- src/main/java/com/qa/hobbyproject/HobbyProjectApplication.java
+++ src/main/java/com/qa/hobbyproject/HobbyProjectApplication.java
@@ -1,4 +1,4 @@
-package com.qa.demo;
+package com.qa.hobbyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/qa/hobbyproject/persistence/domain/Mixes.java
@@ -0,0 +1,5 @@
+package com.qa.hobbyproject.persistence.domain;
+
+public class Mixes {
+
+}
