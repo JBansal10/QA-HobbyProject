5b668e15750aaf789b993163fae471e5fc32be4d
Jbansal10 <jbansal@academytrainee.com>
2021-01-27 12:39:38 +0000

    second enttiy testing begun

--- src/test/java/com/qa/demo/service/MixesServiceUnitTest.java
+++ src/test/java/com/qa/demo/service/MixesServiceUnitTest.java
@@ -21,87 +21,97 @@ import com.qa.hobbyproject.services.MixesService;
 
 @SpringBootTest(classes = HobbyProjectApplication.class)
 public class MixesServiceUnitTest {
-	
+
 	@Autowired
 	private MixesService service;
-	
+
 	@MockBean
 	private MixesRepo repoMock;
-	
-	private List<Mixes> mixList = new ArrayList<>();
+
+	private List<Mixes> mixList;
+	private List<MixesDTO> mixListDTO;
 	private Time time;
 	private MixesDTO mixDTO;
 	private Mixes mixTest;
 	private Long Id;
-	
+
 	@BeforeEach
 	void init() {
 		this.mixList = new ArrayList<>();
+		this.mixListDTO = new ArrayList<>();
 		this.time = Time.valueOf("00:57:39");
 		this.mixDTO = new MixesDTO(1L, time, "Mix number 1", null);
 		this.mixTest = new Mixes(1L, "Mix number 1", time, null);
 		this.mixList.add(mixTest);
+		this.mixListDTO.add(mixDTO);
 		this.Id = 1L;
 	}
-	
+
 	@Test
 	public void createTest() {
 		Mockito.when(this.repoMock.save(Mockito.any(Mixes.class))).thenReturn(this.mixTest);
-		
+
 		MixesDTO result = this.service.create(this.mixTest);
-		
+
 		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(this.mixDTO);
-		
+
 		Mockito.verify(this.repoMock, Mockito.times(1)).save(this.mixTest);
 	}
-	
+
 	@Test
 	public void readAllTest() {
-		
+
 		Mockito.when(this.repoMock.findAll()).thenReturn(this.mixList);
-		
+
 		List<MixesDTO> result = this.service.readAll();
 		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(this.mixList);
-		
+
 		Mockito.verify(this.repoMock, Mockito.times(1)).findAll();
 	}
 
 	@Test
 	public void readUnoTest() {
 		Mockito.when(this.repoMock.findById(this.Id)).thenReturn(Optional.of(this.mixTest));
-		
+
 		MixesDTO result = this.service.readUno(this.Id);
-		
+
 		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(this.mixDTO);
-		
+
 		Mockito.verify(this.repoMock, Mockito.times(1)).findById(this.Id);
 	}
-	
+
 	@Test
 	public void updateTest() {
 		this.Id = 1L;
 		Time updateTime = Time.valueOf("00:58:21");
-		
+
 		Mixes domainUpdate = new Mixes(Id, "Re-Mix number 1", updateTime, null);
 		domainUpdate.setId(Id);
 
 		MixesDTO updatedMixDTO = new MixesDTO(Id, time, "Re-Mix number 1", null);
 		this.mixTest.setId(Id);
-		
+
 		Mockito.when(this.repoMock.findById(1L)).thenReturn(Optional.of(this.mixTest));
 		Mockito.when(this.repoMock.save(domainUpdate)).thenReturn(domainUpdate);
-		
+
 		MixesDTO result = this.service.update(1L, domainUpdate);
-		
+
 		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(updatedMixDTO);
 		Mockito.verify(this.repoMock, Mockito.times(1)).findById(1L);
-		
+
 	}
-	
+
 	@Test
 	public void deleteTest() {
 		Mockito.when(this.repoMock.existsById(Id)).thenReturn(false);
 		Assertions.assertThat(this.service.delete(Id)).isTrue();
+		Mockito.verify(this.repoMock, Mockito.times(1)).existsById(Id);
+	}
+
+	@Test
+	public void deleteTestFail() {
+		Mockito.when(this.repoMock.existsById(Id)).thenReturn(true);
+		Assertions.assertThat(this.service.delete(Id)).isFalse();
 		Mockito.verify(this.repoMock, Mockito.times(1)).existsById(Id);
 	}
 }
---
+++ src/test/java/com/qa/demo/service/TracksServiceUnitTest.java
@@ -0,0 +1,110 @@
+package com.qa.demo.service;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.hobbyproject.HobbyProjectApplication;
+import com.qa.hobbyproject.persistence.domain.Tracks;
+import com.qa.hobbyproject.persistence.dto.TracksDTO;
+import com.qa.hobbyproject.persistence.repos.TracksRepo;
+import com.qa.hobbyproject.services.TracksService;
+
+@SpringBootTest(classes = HobbyProjectApplication.class)
+public class TracksServiceUnitTest {
+
+	@Autowired
+	private TracksService service;
+
+	@MockBean
+	private TracksRepo repoMock;
+
+	private List<Tracks> trackList;
+	private Time time;
+	private TracksDTO trackDTO;
+	private Tracks trackTest;
+	private Long Id;
+
+	@BeforeEach
+	void init() {
+		this.trackList = new ArrayList<>();
+		this.time = Time.valueOf("00:03:29");
+		this.trackDTO = new TracksDTO(Id, time, "Pendulum", "Driver");
+		this.trackTest = new Tracks(Id, time, "Driver", "Pendulum", null);
+		this.Id = 1L;
+		this.trackList.add(trackTest);
+	}
+
+	@Test
+	public void createTest() {
+		Mockito.when(this.repoMock.save(Mockito.any(Tracks.class))).thenReturn(trackTest);
+
+		TracksDTO result = this.service.create(trackTest);
+
+		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(trackTest);
+
+		Mockito.verify(this.repoMock, Mockito.times(1)).save(trackTest);
+	}
+
+	@Test
+	public void readUnoTest() {
+		Mockito.when(this.repoMock.findById(Id)).thenReturn(Optional.of(trackTest));
+
+		TracksDTO result = this.service.readUno(Id);
+
+		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(trackDTO);
+
+		Mockito.verify(this.repoMock, Mockito.times(1)).findById(Id);
+	}
+
+	@Test
+	public void readAllTest() {
+		Mockito.when(this.repoMock.findAll()).thenReturn(trackList);
+
+		List<TracksDTO> result = this.service.readAll();
+		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(trackList);
+
+		Mockito.verify(this.repoMock, Mockito.times(1)).findAll();
+	}
+
+	@Test
+	public void updateTest() {
+		trackTest.setId(1L);
+		Tracks updatedTrack = new Tracks(1L, time, "Track Name", "Track Artist", null);
+		updatedTrack.setId(1L);
+
+		TracksDTO updatedDTO = new TracksDTO(1L, time, "Track Artist", "Track Name");
+
+		Mockito.when(this.repoMock.findById(Id)).thenReturn(Optional.of(trackTest));
+		Mockito.when(this.repoMock.save(updatedTrack)).thenReturn(updatedTrack);
+
+		TracksDTO result = this.service.update(Id, updatedTrack);
+
+		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(updatedDTO);
+		Mockito.verify(this.repoMock, Mockito.times(1)).findById(Id);
+	}
+
+	@Test
+	public void deleteTest() {
+		Mockito.when(this.repoMock.existsById(Id)).thenReturn(false);
+		Assertions.assertThat(this.service.delete(Id)).isTrue();
+		Mockito.verify(this.repoMock, Mockito.times(1)).existsById(Id);
+	}
+
+	@Test
+	public void deleteTestFail() {
+		Mockito.when(this.repoMock.existsById(Id)).thenReturn(true);
+		Assertions.assertThat(this.service.delete(Id)).isFalse();
+		Mockito.verify(this.repoMock, Mockito.times(1)).existsById(Id);
+	}
+
+}
